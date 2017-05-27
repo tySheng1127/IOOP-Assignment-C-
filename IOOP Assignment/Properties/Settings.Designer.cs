96da10ecb73a5c7668269b0be1390a5cbd9563eb
tySheng1127 <sean.tan98765@gmail.com>
2017-05-27 11:06:14 +0800

    noob

--- IOOP Assignment/Properties/Settings.Designer.cs
+++ IOOP Assignment/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace IOOP_Assignment.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Cashier.mdf;Integrat" +
-            "ed Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\ioopassignment.mdf;I" +
+            "ntegrated Security=True;Connect Timeout=30")]
         public string CashierConnectionString {
             get {
                 return ((string)(this["CashierConnectionString"]));
