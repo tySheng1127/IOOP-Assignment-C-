4835e9acc4d516ce6bc4f74a7aace9da7e3ec71c
tySheng1127 <sean.tan98765@gmail.com>
2017-06-03 02:55:47 +0800

    employee list

--- IOOP Assignment/EmployeeList.Designer.cs
+++ IOOP Assignment/EmployeeList.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DGV_EmployeeList = new System.Windows.Forms.DataGridView();
             this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.realNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -45,16 +45,43 @@
             this.databaseDataSet = new IOOP_Assignment.DatabaseDataSet();
             this.employeeTableAdapter = new IOOP_Assignment.DatabaseDataSetTableAdapters.EmployeeTableAdapter();
             this.btn_back = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_Realname = new System.Windows.Forms.TextBox();
+            this.txt_NRIC = new System.Windows.Forms.TextBox();
+            this.txt_DOB = new System.Windows.Forms.TextBox();
+            this.txt_Contact = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.txt_Bank = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb_Jobtitle = new System.Windows.Forms.ComboBox();
+            this.cb_Gender = new System.Windows.Forms.ComboBox();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_EmployeeList)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DGV_EmployeeList
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DGV_EmployeeList.AutoGenerateColumns = false;
+            this.DGV_EmployeeList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_EmployeeList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.employeeIDDataGridViewTextBoxColumn,
             this.usernameDataGridViewTextBoxColumn,
             this.realNameDataGridViewTextBoxColumn,
@@ -66,11 +93,12 @@
             this.bankInfoDataGridViewTextBoxColumn,
             this.passwordDataGridViewTextBoxColumn,
             this.jobTitleDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.employeeBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1142, 191);
-            this.dataGridView1.TabIndex = 0;
+            this.DGV_EmployeeList.DataSource = this.employeeBindingSource;
+            this.DGV_EmployeeList.Location = new System.Drawing.Point(12, 12);
+            this.DGV_EmployeeList.Name = "DGV_EmployeeList";
+            this.DGV_EmployeeList.Size = new System.Drawing.Size(1142, 191);
+            this.DGV_EmployeeList.TabIndex = 0;
+            this.DGV_EmployeeList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_EmployeeList_CellContentClick);
             // 
             // employeeIDDataGridViewTextBoxColumn
             // 
@@ -154,34 +182,284 @@
             // 
             // btn_back
             // 
-            this.btn_back.Location = new System.Drawing.Point(506, 222);
+            this.btn_back.Location = new System.Drawing.Point(511, 400);
             this.btn_back.Name = "btn_back";
             this.btn_back.Size = new System.Drawing.Size(139, 36);
-            this.btn_back.TabIndex = 1;
+            this.btn_back.TabIndex = 27;
             this.btn_back.Text = "Back";
             this.btn_back.UseVisualStyleBackColor = true;
             this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EmployeeID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Real Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "NRIC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(355, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Date of Birth";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(349, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Contact Number";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(355, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Address";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(636, 31);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Bank Info";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(636, 73);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Password";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(636, 112);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Job Title";
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Location = new System.Drawing.Point(101, 26);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(202, 20);
+            this.txt_ID.TabIndex = 13;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(101, 63);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(202, 20);
+            this.txt_username.TabIndex = 14;
+            // 
+            // txt_Realname
+            // 
+            this.txt_Realname.Location = new System.Drawing.Point(101, 105);
+            this.txt_Realname.Name = "txt_Realname";
+            this.txt_Realname.Size = new System.Drawing.Size(202, 20);
+            this.txt_Realname.TabIndex = 15;
+            // 
+            // txt_NRIC
+            // 
+            this.txt_NRIC.Location = new System.Drawing.Point(101, 147);
+            this.txt_NRIC.Name = "txt_NRIC";
+            this.txt_NRIC.Size = new System.Drawing.Size(202, 20);
+            this.txt_NRIC.TabIndex = 16;
+            // 
+            // txt_DOB
+            // 
+            this.txt_DOB.Location = new System.Drawing.Point(444, 70);
+            this.txt_DOB.Name = "txt_DOB";
+            this.txt_DOB.Size = new System.Drawing.Size(169, 20);
+            this.txt_DOB.TabIndex = 18;
+            // 
+            // txt_Contact
+            // 
+            this.txt_Contact.Location = new System.Drawing.Point(444, 104);
+            this.txt_Contact.Name = "txt_Contact";
+            this.txt_Contact.Size = new System.Drawing.Size(169, 20);
+            this.txt_Contact.TabIndex = 19;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(444, 147);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(169, 20);
+            this.txt_Address.TabIndex = 20;
+            // 
+            // txt_Bank
+            // 
+            this.txt_Bank.Location = new System.Drawing.Point(719, 24);
+            this.txt_Bank.Name = "txt_Bank";
+            this.txt_Bank.Size = new System.Drawing.Size(176, 20);
+            this.txt_Bank.TabIndex = 21;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(719, 66);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(176, 20);
+            this.txt_Password.TabIndex = 22;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb_Jobtitle);
+            this.groupBox1.Controls.Add(this.cb_Gender);
+            this.groupBox1.Controls.Add(this.btn_Delete);
+            this.groupBox1.Controls.Add(this.btn_Insert);
+            this.groupBox1.Controls.Add(this.txt_Bank);
+            this.groupBox1.Controls.Add(this.txt_Password);
+            this.groupBox1.Controls.Add(this.btn_Update);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txt_DOB);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txt_Contact);
+            this.groupBox1.Controls.Add(this.txt_Address);
+            this.groupBox1.Controls.Add(this.txt_ID);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_username);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txt_NRIC);
+            this.groupBox1.Controls.Add(this.txt_Realname);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(13, 210);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1141, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Manage Employee";
+            // 
+            // cb_Jobtitle
+            // 
+            this.cb_Jobtitle.FormattingEnabled = true;
+            this.cb_Jobtitle.Items.AddRange(new object[] {
+            "Employee",
+            "Supervisor"});
+            this.cb_Jobtitle.Location = new System.Drawing.Point(719, 112);
+            this.cb_Jobtitle.Name = "cb_Jobtitle";
+            this.cb_Jobtitle.Size = new System.Drawing.Size(176, 21);
+            this.cb_Jobtitle.TabIndex = 28;
+            // 
+            // cb_Gender
+            // 
+            this.cb_Gender.FormattingEnabled = true;
+            this.cb_Gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cb_Gender.Location = new System.Drawing.Point(444, 26);
+            this.cb_Gender.Name = "cb_Gender";
+            this.cb_Gender.Size = new System.Drawing.Size(169, 21);
+            this.cb_Gender.TabIndex = 27;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(958, 137);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(156, 38);
+            this.btn_Delete.TabIndex = 26;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(958, 83);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(156, 34);
+            this.btn_Insert.TabIndex = 25;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(958, 21);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(156, 39);
+            this.btn_Update.TabIndex = 24;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
             // 
             // EmployeeList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1166, 265);
+            this.ClientSize = new System.Drawing.Size(1166, 448);
             this.Controls.Add(this.btn_back);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DGV_EmployeeList);
+            this.Controls.Add(this.groupBox1);
             this.Name = "EmployeeList";
             this.Text = "EmployeeList";
-            this.Load += new System.EventHandler(this.EmployeeList_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_EmployeeList)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DGV_EmployeeList;
         private DatabaseDataSet databaseDataSet;
         private System.Windows.Forms.BindingSource employeeBindingSource;
         private DatabaseDataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
@@ -197,5 +475,31 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn jobTitleDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_Realname;
+        private System.Windows.Forms.TextBox txt_NRIC;
+        private System.Windows.Forms.TextBox txt_DOB;
+        private System.Windows.Forms.TextBox txt_Contact;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.TextBox txt_Bank;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.ComboBox cb_Gender;
+        private System.Windows.Forms.ComboBox cb_Jobtitle;
     }
 }
--- IOOP Assignment/EmployeeList.cs
+++ IOOP Assignment/EmployeeList.cs
@@ -7,26 +7,149 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace IOOP_Assignment
 {
     public partial class EmployeeList : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
         public EmployeeList()
         {
             InitializeComponent();
+            DisplayData();
         }
 
-        private void EmployeeList_Load(object sender, EventArgs e)
+        private void DisplayData()//display data in data grid view
         {
-            // TODO: This line of code loads data into the 'databaseDataSet.Employee' table. You can move, or remove it, as needed.
-            this.employeeTableAdapter.Fill(this.databaseDataSet.Employee);
-
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from Employee", con);
+            adapt.Fill(dt);
+            DGV_EmployeeList.DataSource = dt;
+            con.Close();
         }
+
+        //Clear Data in Manage Employee 
+        private void ClearData()
+        {
+            txt_ID.Text = "";
+            txt_username.Text = "";
+            txt_Realname.Text = "";
+            txt_NRIC.Text = "";
+            cb_Gender.Text = "";
+            txt_DOB.Text = "";
+            txt_Contact.Text = "";
+            txt_Address.Text = "";
+            txt_Bank.Text = "";
+            txt_Password.Text = "";
+            cb_Jobtitle.Text = "";
+        }  
+
+       
 
         private void btn_back_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            if (txt_ID.Text!=""&&txt_username.Text!=""&&txt_Realname.Text!=""&&txt_NRIC.Text!=""&&cb_Gender.Text!=""&&txt_DOB.Text!=""&&txt_Contact.Text!=""&&txt_Address.Text!=""&&txt_Bank.Text!=""&&txt_Password.Text!=""&&cb_Jobtitle.Text!="")
+            {
+                cmd = new SqlCommand("update Employee set username=@usrname, realName=@name, NRIC=@IC,Gender=@gender,DoB=@dob,ContactNumber=@contact,Address=@address,BankInfo=@bank,password=@pw,Jobtitle=@title where EmployeeID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", txt_ID.Text);
+                cmd.Parameters.AddWithValue("@usrname", txt_username.Text);
+                cmd.Parameters.AddWithValue("@name", txt_Realname.Text);
+                cmd.Parameters.AddWithValue("@IC", txt_NRIC.Text);
+                cmd.Parameters.AddWithValue("@gender", cb_Gender.Text);
+                cmd.Parameters.AddWithValue("@dob", DateTime.Parse(txt_DOB.Text));
+                cmd.Parameters.AddWithValue("@contact", txt_Contact.Text);
+                cmd.Parameters.AddWithValue("@address", txt_Address.Text);
+                cmd.Parameters.AddWithValue("@bank", txt_Bank.Text);
+                cmd.Parameters.AddWithValue("@pw", txt_Password.Text);
+                cmd.Parameters.AddWithValue("@title", cb_Jobtitle.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("EmployeeList is updated !");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please select the Employee to update !");
+            }
+        }
+
+        private void DGV_EmployeeList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            txt_ID.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txt_username.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_Realname.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txt_NRIC.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[3].Value.ToString();
+            cb_Gender.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txt_DOB.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txt_Contact.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[6].Value.ToString();
+            txt_Address.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[7].Value.ToString();
+            txt_Bank.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[8].Value.ToString();
+            txt_Password.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[9].Value.ToString();
+            cb_Jobtitle.Text = DGV_EmployeeList.Rows[e.RowIndex].Cells[10].Value.ToString();
+            
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            if (txt_ID.Text != "" && txt_username.Text != "" && txt_Realname.Text != "" && txt_NRIC.Text != "" && cb_Gender.Text != "" && txt_DOB.Text != "" && txt_Contact.Text != "" && txt_Address.Text != "" && txt_Bank.Text != "" && txt_Password.Text != "" && cb_Jobtitle.Text != "")
+            {
+                cmd = new SqlCommand("insert into Employee(EmployeeID,username,realName,NRIC,Gender,DoB,ContactNumber,Address,BankInfo,password,JobTitle) values(@id,@usrname,@name,@IC,@gender,@dob,@contact,@address,@bank,@pw,@title)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", txt_ID.Text);
+                cmd.Parameters.AddWithValue("@usrname", txt_username.Text);
+                cmd.Parameters.AddWithValue("@name", txt_Realname.Text);
+                cmd.Parameters.AddWithValue("@IC", txt_NRIC.Text);
+                cmd.Parameters.AddWithValue("@gender", cb_Gender.Text);
+                cmd.Parameters.AddWithValue("@dob", DateTime.Parse(txt_DOB.Text));
+                cmd.Parameters.AddWithValue("@contact", txt_Contact.Text);
+                cmd.Parameters.AddWithValue("@address", txt_Address.Text);
+                cmd.Parameters.AddWithValue("@bank", txt_Bank.Text);
+                cmd.Parameters.AddWithValue("@pw", txt_Password.Text);
+                cmd.Parameters.AddWithValue("@title", cb_Jobtitle.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("New Employee is inserted !");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all the details !");
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (txt_ID.Text != "")
+            {
+                cmd = new SqlCommand("delete from Employee where EmployeeID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", txt_ID.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record deleted !");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please select the item to delete !");
+            }
+        }
+
+
+        
     }
 }
